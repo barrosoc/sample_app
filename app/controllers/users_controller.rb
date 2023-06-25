e3ecf5b1651af17b801c345f12962cdf151c51ff
barrosoc <carlos.barroso.baltasar@gmail.com>
2023-06-25 17:21:47 +0200

    Add integration test for a valid signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else 
-      flash[:error] = @user.errors.full_messages
       render 'new', status: :unprocessable_entity
     end
   end
